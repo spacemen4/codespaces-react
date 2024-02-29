a4c140eff215b951b07d6b15d2dcd8a279085dbd
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2024-02-29 22:49:22 +0000

    fvfdgbvfdgdffffgf

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,23 @@ import ClinicalInformationForm from './components/clinicalinformation';
 import Form from './components/coordonnee';
 import PaymentForm from './components/middleform';
 import SampleForm from './components/sample';
+import {
+  Box,
+} from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
+    <Box>
+      <Box>
       <Form/>
       <SampleForm/>
+      </Box>
       <PaymentForm/>
       <ClinicalInformationForm/>
       <AssessmentForm/>
       <PrescriberForm/>
       <ScanningInstructionsForm/>
-    </div>
+    </Box>
   );
 }
 
