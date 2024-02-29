29e3758eb3063022ab2a02d17c96e91e48521ecc
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2024-02-29 22:17:29 +0000

    gggggggggggggggggggg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import AssessmentForm from './AssessmentForm';
-import PrescriberForm from './PrescriberForm';
-import ScanningInstructionsForm from './ScanningInstructionsForm';
-import ClinicalInformationForm from './clinicalinformation';
-import Form from './coordonnee';
-import PaymentForm from './middleform';
-import SampleForm from './sample';
+import AssessmentForm from './components/AssessmentForm';
+import PrescriberForm from './components/PrescriberForm';
+import ScanningInstructionsForm from './components/ScanningInstructionsForm';
+import ClinicalInformationForm from './components/clinicalinformation';
+import Form from './components/coordonnee';
+import PaymentForm from './components/middleform';
+import SampleForm from './components/sample';
 
 function App() {
   return (
